d6e0306d26e9fa9790dc1b3fd7b265776e1b2cd9
Timothy Danford <tdanford@gmail.com>
2014-11-30 07:26:27 -0500

    Updating the Postprocessor trait, finishing the ProximalGapFilter.

--- avocado-core/src/main/scala/org/bdgenomics/avocado/postprocessing/mutect/MutectPostprocessor.scala
+++ avocado-core/src/main/scala/org/bdgenomics/avocado/postprocessing/mutect/MutectPostprocessor.scala
@@ -18,31 +18,71 @@
 
 package org.bdgenomics.avocado.postprocessing.mutect
 
+import org.apache.spark.SparkContext._
 import org.apache.spark.rdd.RDD
-import org.bdgenomics.adam.models.VariantContext
+import org.bdgenomics.adam.models.{ReferenceRegion, ReferenceMapping, VariantContext}
 import org.bdgenomics.formats.avro.AlignmentRecord
 import org.bdgenomics.adam.rich.ReferenceMappingContext._
 import ClassifiedContext._
 import org.bdgenomics.adam.rdd.RegionRDDFunctions._
+import scala.math.max
 
 trait MutectPostprocessor extends Serializable {
 
   def filter(variants: RDD[VariantContext],
-             reads: RDD[Classified[AlignmentRecord]]): RDD[VariantContext]
-
+             tumorReads: RDD[Classified[AlignmentRecord]],
+             normalReads: RDD[Classified[AlignmentRecord]]): RDD[VariantContext]
 }
 
-class ProximalIndelFilter(val indelReadThreshold: Int = 3,
-                          val windowDistance: Int = 11) extends MutectPostprocessor with Serializable {
+object ProximalGapFilter {
 
-  def hasIndel(rec: AlignmentRecord): Boolean = ???
+  /**
+   * Different descriptions of Mutect have different descriptions of this filter --
+   * it's not clear whether it's
+   *   (>= 3 reads with either an insertion or a deletion)
+   * or
+   *   (>= 3 reads with an insertion) or (>= 3 reads with a deletion)
+   *
+   * The paper seems to suggest the latter, but other sources (includes slides I've found)
+   * suggest the former.
+   *
+   * So I'm abstracting it out, with a simple implementation here that can be parameterized
+   * later.
+   */
+  def hasIndel(rec: AlignmentRecord): Boolean =
+    rec.getCigar.contains("I") || rec.getCigar.contains("D")
+}
+
+
+/**
+ * ProximalGapFilter implements the HC filter described by (in the paper) the following
+ * text:
+ *   Remove false positives caused by nearby misaligned small insertion and deletion events.
+ *   Reject candidate site if there are ≥ 3 reads with insertions in an 11-base-pair window
+ *   centered on the candidate mutation or if there are ≥ 3 reads with deletions in the same
+ *   11-base-pair window.
+ *
+ * See the notes to the ProximalGapFilter.hasIndel method, above, for more details on some
+ * ambiguity in this statement.
+ *
+ * @param indelReadThreshold The number of reads passing the filter below which the variant is
+ *                           retained (by default == 3)
+ * @param windowDistance The distance around the variant to look for overlapping reads
+ *                       (by default == 11)
+ * @param gapFilter The function that determines whether or not a read has an indel.
+ */
+class ProximalGapFilter(val indelReadThreshold: Int = 3,
+                        val windowDistance: Int = 11,
+                        val gapFilter : AlignmentRecord=>Boolean = ProximalGapFilter.hasIndel)
+extends MutectPostprocessor with Serializable {
 
   override def filter(variants: RDD[VariantContext],
-                      reads: RDD[Classified[AlignmentRecord]]): RDD[VariantContext] =
+                      tumorReads: RDD[Classified[AlignmentRecord]],
+                       normalReads : RDD[Classified[AlignmentRecord]]): RDD[VariantContext] =
     variants
       .groupWithinRange(
-        reads.filterByClasses("tumor", "retained").values(),
+        tumorReads.filterByClasses("retained").values(),
         windowDistance.toLong)
-      .filter(_._2.count(hasIndel) < indelReadThreshold)
+      .filter(_._2.count(gapFilter) < indelReadThreshold)
       .map(_._1)
 }
