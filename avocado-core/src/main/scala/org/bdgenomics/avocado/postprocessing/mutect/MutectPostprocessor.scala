7f20fd1f100a96884a0fb79592597f03b159f4e0
Timothy Danford <tdanford@gmail.com>
2014-11-30 14:24:35 -0500

    Starting the TriallelicFilter

--- avocado-core/src/main/scala/org/bdgenomics/avocado/postprocessing/mutect/MutectPostprocessor.scala
+++ avocado-core/src/main/scala/org/bdgenomics/avocado/postprocessing/mutect/MutectPostprocessor.scala
@@ -145,3 +145,26 @@ class PoorMappingFilter(val mapq0Fraction : Double=0.5,
     filter1.intersection(filter2)
   }
 }
+
+/**
+ * Based on the following text:
+ *   Reject false positives caused by calling triallelic sites where the normal
+ *   sample is heterozygous with alleles A and B, and MuTect is considering an
+ *   alternate allele C. Although this is biologically possible, and remains an
+ *   area for future improvement in the detection of mutations, calling at these
+ *   sites generates many false positives, and therefore they are currently
+ *   filtered out by default. However, it may be desirable to review mutations
+ *   failing only this filter for biological relevance and orthogonal validation,
+ *   and to study the underlying reasons for these false positives.
+ */
+class TriallelicSiteFilter extends MutectPostprocessor with Serializable {
+
+  def isTriAllelic(vc : VariantContext, normalReads : Iterable[AlignmentRecord]) : Boolean =
+    ???
+
+  override def filter(variants: RDD[VariantContext],
+                      tumorReads: RDD[Classified[AlignmentRecord]],
+                      normalReads: RDD[Classified[AlignmentRecord]]): RDD[VariantContext] = {
+    variants.groupByOverlap()
+  }
+}
